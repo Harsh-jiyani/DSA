1f1a5094b5181fdd2cb1228d5bd1b6e9985cab32
Harsh Jiyani <harshjiyani4277@gmail.com>
2025-04-04 00:35:44 +0530

    Link List

---
+++ link_list.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class LinkedList {
+public:
+    Node* head;
+
+    LinkedList() {
+        head = nullptr;
+    }
+
+    // Insert a new node at the end
+    void insert(int val) {
+        Node* newNode = new Node();
+        newNode->data = val;
+        newNode->next = nullptr;
+
+        if (head == nullptr) {
+            head = newNode;
+        } else {
+            Node* temp = head;
+            while (temp->next != nullptr) {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+
+    // Display the linked list
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        cout << "NULL" << endl;
+    }
+};
+
+int main() {
+    LinkedList list;
+    list.insert(10);
+    list.insert(20);
+    list.insert(30);
+
+    cout << "Linked List: ";
+    list.display();
+
+    return 0;
+}
